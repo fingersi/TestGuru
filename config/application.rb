992ab4fff664829f5331480f80efee821758cdb6
Алексей <alyoshinaa@gmail.com>
2021-07-10 20:12:46 +0300

    added static pages and app discription

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,8 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Testguru
+module TestGuru
+  VERSION = '0.0.1'
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -16,7 +17,9 @@ module Testguru
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Moscow"
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :en
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
