8ecec50ffd27055f37fc93d961087ec864706aa9
Алексей <alyoshinaa@gmail.com>
2021-10-06 13:26:12 +0300

    fixs

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,10 +2,11 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :user_histories
   has_many :users, through: :user_histories
+  has_many :questions
 
   def self.find_by_category(category)
     Test.joins(:category)
         .where('categories.title = ?', category)
-        .order('tests.title DESC')
+        .order('tests.title DESC').pluck(:title)
   end
 end
