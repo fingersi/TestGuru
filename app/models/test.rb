0a1b5c9061b3377eed8d3ef6d9e6e337a2dce1eb
Алексей <alyoshinaa@gmail.com>
2021-11-01 23:38:49 +0300

    added scopes methoder to Test and category models

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,11 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :questions
 
+  scope :by_level, ->(lev) { where(level: lev) }
+  scope :easy, -> { by_level(0..1) }
+  scope :moderate, -> { by_level(2..4) }
+  scope :advance, -> { by_level(4..Float::INFINITY) }
+
   def self.find_by_category(category)
     Test.joins(:category)
         .where('categories.title = ?', category)
