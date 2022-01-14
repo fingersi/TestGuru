55696226993fbd4ceac20b7a5d9b4fa7a16bd16c
Алексей <alyoshinaa@gmail.com>
2022-01-14 14:13:55 +0300

    fixes

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,9 +1,8 @@
 module FlashHelper
-  def aaa
+  def flash_alerts
     msgs = ''
     flash.each do |type, msg|
-      puts "msg= #{msg}"
-      msgs << content_tag(:p, "#{type} : #{msg}", class: 'flash alert')
+      msgs += content_tag(:p, "#{type} : #{msg}", class: 'flash alert')
     end
     msgs.html_safe
   end
