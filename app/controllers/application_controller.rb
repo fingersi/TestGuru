cecf3a61ae9cd3d22f798b718f3cd1e2d9721676
Алексей <alyoshinaa@gmail.com>
2022-03-27 10:41:14 +0300

    localized expect devise, added url changer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,13 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+  before_action :set_locale
 
   private
+
+  def default_url_options
+    { lang: set_url }
+  end
 
   def after_sign_in_path_for(resource_or_scope)
     resource_or_scope.admin? ? admin_tests_path : tests_path
@@ -14,5 +19,14 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
+  end
+
+  def set_url
+    puts "params[:lang]&.to_sym == I18n.default_locale #{params[:lang]&.to_sym == I18n.default_locale}"
+    params[:lang]&.to_sym == I18n.default_locale ? nil : I18n.locale
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,17 +2,17 @@ module ApplicationHelper
 
   def user_name
     if user_signed_in?
-      content_tag :p, "Welcome, #{current_user.login} Guru", class: 'nav link'
+      content_tag :p, t('menu.welcome', user: current_user.login), class: 'nav link'
     else
-      link_to 'Register', new_user_registration_path, class: 'nav link'
+      link_to t('menu.register'), new_user_registration_path, class: 'nav link'
     end
   end
 
   def log_in_log_out
     if user_signed_in?
-      link_to 'Logout', destroy_user_session_path, method: :delete, class: 'nav link'
+      link_to t('menu.logout'), destroy_user_session_path, method: :delete, class: 'nav link'
     else
-      link_to 'Login', new_user_session_path, class: 'nav link'
+      link_to t('menu.login'), new_user_session_path, class: 'nav link'
     end
   end
 end
