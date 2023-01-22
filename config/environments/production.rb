390f033efb4856ed01b468025a0fbdc7b7c309b2
Алексей <alyoshinaa@gmail.com>
2023-01-22 20:13:34 +0300

    added smtp production settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,25 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address:              '',
+    port:                 25,
+    #domain:               '',
+    user_name:            '',
+    password:             '',
+    #authentication:       'plain',
+    #enable_starttls_auto: true,
+    open_timeout:         15,
+    read_timeout:         15
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
