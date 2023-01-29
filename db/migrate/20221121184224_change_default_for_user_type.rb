d4b666779e90b9c425bdceb22bca5f57ea56f065
Алексей <alyoshinaa@gmail.com>
2023-01-29 13:02:05 +0300

    change migration for users

--- db/migrate/20221121184224_change_default_for_user_type.rb
+++ db/migrate/20221121184224_change_default_for_user_type.rb
@@ -1,5 +1,9 @@
 class ChangeDefaultForUserType < ActiveRecord::Migration[6.1]
-  def change
+  def self.up
     change_column_default :users, :type, "User"
+  end
+
+  def self.down
+    change_column_default :users, :type, "user"
   end
 end
