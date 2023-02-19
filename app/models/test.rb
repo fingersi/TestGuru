937e7d72f847c552093636a6f57b80cf575572fa
Алексей <alyoshinaa@gmail.com>
2023-02-19 23:43:55 +0300

    added depend destroy and scope for_users

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,32 +1,27 @@
 class Test < ApplicationRecord
   belongs_to :category
-  has_many :test_passing
+  has_many :test_passing, dependent: :delete_all
   has_many :users, through: :test_passing
   belongs_to :author, class_name: 'User'
-  has_many :questions
+  has_many :questions, dependent: :destroy
 
   scope :by_level, ->(lev) { where(level: lev) }
   scope :easy, -> { by_level(0..1) }
   scope :moderate, -> { by_level(2..4) }
   scope :advance, -> { by_level(4..Float::INFINITY) }
   scope :find_by_category, ->(category) { Test.joins(:category).where('categories.title = ?', category) }
+  scope :for_users, -> { where(published: true) }
 
   validates :title, presence: true, uniqueness: { scope: :level }
   validates :level, numericality: { other_than: 0 }
   validate :published, :check_question
 
-  before_destroy :destroy_associations
-
   def self.find_order_by_category(category)
     Test.find_by_category(category).order('tests.title DESC').pluck(:title)
   end
 
-  def self.for_users
-    Test.where(published: true)
-  end
-
   def check_question
-    if published && fullfilled?
+    if published && !fullfilled?
 
       errors.add :fullfilled, :no_question_answers, message: I18n.t('.cannot_publish_no_question_or_answer')
       return false
